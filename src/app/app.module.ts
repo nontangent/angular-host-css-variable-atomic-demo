a588c95ae19b26c1727e1897fa1ae9d879bc65c6
nontangent <irregular0number@gmail.com>
2021-01-27 13:15:19 +0900

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AtomComponent } from './atom/atom.component';
+import { MoleculeComponent } from './molecule/molecule.component';
+import { OrganismComponent } from './organism/organism.component';
+import { TemplateComponent } from './template/template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AtomComponent,
+    MoleculeComponent,
+    OrganismComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule
